0f7eeae8f43389f9489ab6d18e97f07969f2acd6
Mays07 <maysderrick898@gmail.com>
2026-06-23 08:53:31 -0500

    please save

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function buttonPress(){
+    let navIframe = document.getElementsById('nav');
+    navIframe.style.width=1080+'px';
+    navIframe.style.height=300+'px';
+    navIframe.setAttribute('src', 'nav.html');
+}

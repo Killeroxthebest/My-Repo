4f27e3bfbf12eba6c0ca0efc21d5bffb89edf523
Mays07 <maysderrick898@gmail.com>
2026-06-23 14:55:20 -0500

    nav fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function buttonPress(){
-    let navIframe = document.getElementsById('nav');
+    let navIframe = document.getElementsByClassName('nav');
     navIframe.style.width=1080+'px';
-    navIframe.style.height=300+'px';
+    navIframe.style.height=100+'px';
     navIframe.setAttribute('src', 'nav.html');
 }

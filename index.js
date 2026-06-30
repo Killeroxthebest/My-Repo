46e77a11bf8c5a82675bb817c15a5c4e75ab5e21
Mays07 <maysderrick898@gmail.com>
2026-06-30 17:40:52 -0500

    list

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ let button1 = document.querySelector(".option-one");
 let button2 = document.querySelector(".option-two");
 let carImage = document.querySelector(".cImage");
 
-let images = ["asset/Mays_2.jpg", "asset/nai.png"]
+let images = ["asset/Mays_2.jpg",
+    "asset/4563.jpg",
+    "asset/4640.jpg",
+    "asset/4641.jpg",
+    "asset/4645.jpg"]
 let imageNum = 0;
 button1.addEventListener('click', function () {
     imageNum = imageNum - 1;
     if (imageNum < 0) {
-        imageNum = images.length-1
+        imageNum = 4
     }
-    carImage.src=images[imageNum]
+    carImage.src = images[imageNum]
 });
 button2.addEventListener('click', function () {
     imageNum = imageNum + 1
-    if (imageNum > images.length-1) {
+    if (imageNum > 4) {
         imageNum = 0
     }
-   carImage.src=images[imageNum]
+    carImage.src = images[imageNum]
 });

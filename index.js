a387e24ea1d029ace87dc0be0817cd391e3392da
Mays07 <maysderrick898@gmail.com>
2026-06-30 12:46:53 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 
-function buttonPress(){
+function buttonPress() {
     let navIframe = document.getElementsByClassName('nav');
-    navIframe.style.width=1080+'px';
-    navIframe.style.height=100+'px';
+    navIframe.style.width = 1080 + 'px';
+    navIframe.style.height = 100 + 'px';
     navIframe.setAttribute('src', 'nav.html');
 }
+// js for buttons and images
+let button1 = document.querySelector(".option-one");
+let button2 = document.querySelector(".option-two");
+let carImage = document.querySelector(".cImage");
+
+let images = ["asset/Mays_2.jpg", "asset/nai.png"]
+let imageNum = 0;
+button1.addEventListener('click', function () {
+    imageNum = imageNum - 1;
+    if (imageNum < 0) {
+        imageNum = images.length-1
+    }
+    carImage.src=images[imageNum]
+});
+button2.addEventListener('click', function () {
+    imageNum = imageNum + 1
+    if (imageNum > images.length-1) {
+        imageNum = 0
+    }
+   carImage.src=images[imageNum]
+});
